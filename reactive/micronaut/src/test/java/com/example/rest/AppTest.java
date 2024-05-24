3ee8aed8bdba1efd672c3b54d123d19707cba6b0
sullis <github@seansullivan.com>
2024-05-23 19:08:03 -0700

    micronaut: enable netty native transports

--- reactive/micronaut/src/test/java/com/example/rest/AppTest.java
+++ reactive/micronaut/src/test/java/com/example/rest/AppTest.java
@@ -2,10 +2,15 @@ package com.example.rest;
 
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import io.netty.channel.epoll.Epoll;
+import io.netty.channel.kqueue.KQueue;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 import jakarta.inject.Inject;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
 
 @MicronautTest
 class AppTest {
@@ -18,4 +23,15 @@ class AppTest {
         Assertions.assertTrue(application.isRunning());
     }
 
+    @Test
+    @EnabledOnOs(value = OS.MAC)
+    void kqueueIsAvailableOnMacOs() {
+        KQueue.ensureAvailability();
+    }
+
+    @Test
+    @EnabledOnOs(value = OS.LINUX)
+    void epollIsAvailableOnLinux() {
+        Epoll.ensureAvailability();
+    }
 }
